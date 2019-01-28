676ecc25ff6307c2100d94d2abf47a75ee4a3fe1
KateWallet <katewallet95@gmail.com>
2019-01-28 04:27:46 +0300

    Initial commit

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -33,9 +33,9 @@ AppAsset::register($this);
         <a href="/" id="logo" title="Diana’s jewelry">Diana’s jewelry</a>
         <div class="right-links">
             <ul>
-                <li><a href="cart.html"><span class="ico-products"></span>3 products, $4 500.00</a></li>
-                <li><a href="#"><span class="ico-account"></span>Account</a></li>
-                <li><a href="#"><span class="ico-signout"></span>Sign out</a></li>
+                <li><a href="cart.html"><span class="ico-products"></span>Количество: 3 Сумма: $4 500.00</a></li>
+                <li><a href="#"><span class="ico-account"></span>Аккаунт</a></li>
+                <li><a href="#"><span class="ico-signout"></span>Выйти</a></li>
             </ul>
         </div>
     </div>
